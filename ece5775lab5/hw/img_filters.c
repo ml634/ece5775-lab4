4aa7a89d1c97ee891c08c80129f64d53f376a802
Andrew Palmer <ajp294@cornell.edu>
2016-12-08 17:08:45 -0500

    one of the pixel arrays had not been changed to char yet

--- ece5775lab5/hw/img_filters.c
+++ ece5775lab5/hw/img_filters.c
@@ -39,13 +39,7 @@ char yc_data_segmented[NUMROWS*NUMCOLS];
 char yc_data_segmented_out[NUMROWS*NUMCOLS]; 
 char yc_data_segmented_out_filter[NUMROWS*NUMCOLS]; 
 
-
-// unsigned short yc_data_red_out_filtered[NUMROWS*NUMCOLS];
-// unsigned short yc_data_blue_out_filtered[NUMROWS*NUMCOLS];  
-// unsigned short yc_data_green_out_filtered[NUMROWS*NUMCOLS];
-
-unsigned short yc_data_combined[NUMROWS*NUMCOLS];
-unsigned short yc_data_combined_filter[NUMROWS*NUMCOLS];
+char yc_data_combined[NUMROWS*NUMCOLS];
 
 // local arrays to hold blue and red robot center of mass points
 // unsigned int blueCOM[2];
@@ -67,20 +61,19 @@ void img_process( unsigned int *rgb_data_in, unsigned int *rgb_data_out, unsigne
 
    // median filter
    // median_char_filter_pass(yc_data_segmented_out,yc_data_segmented_out_filter );
-   median_char_filter_pass(yc_data_segmented, yc_data_segmented_out_filter );
+   // median_char_filter_pass(yc_data_segmented, yc_data_segmented_out_filter );
 
    // calculate the center of mass for red robot, blue robot, and green goal
-   centerOfMass(yc_data_segmented_out_filter, yc_data_segmented_out, frame_com);
+   centerOfMass(yc_data_segmented, yc_data_segmented_out, frame_com);
 
    // Process image to capture corners for both robots
    //call median filter for each RBG --> and corner detect for each filtered output RBG
- 
 
    // FINDME: Bypassing corner detect to test median filter
-   // corner_detect( yc_data_segmented_out , yc_data_combined, frame_corners);
+   corner_detect( yc_data_segmented_out , yc_data_combined, frame_corners);
       
    // ycbcr2rgb_pad(yc_data_combined, rgb_data_out, frame_com ,frame_corners );
    // ycbcr2rgb_pad(yc_data_segmented_out_filter, rgb_data_out, frame_com ,frame_corners );
-   ycbcr2rgb_pad(yc_data_segmented_out, rgb_data_out, frame_com ,frame_corners );
+   ycbcr2rgb_pad(yc_data_combined, rgb_data_out, frame_com ,frame_corners );
 
 }
